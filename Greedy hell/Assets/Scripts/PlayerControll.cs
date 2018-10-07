23954f4cf7e36014f6fd5a110ba6525ab994a4d7
xwk <570312124@qq.com>
2018-10-07 14:40:51 +0800

    添加玩家移动脚本

---
+++ Greedy hell/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed = 3.0f;
+    private float v, h;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(h, v, 0) * moveSpeed * Time.deltaTime);
+	}
+}
