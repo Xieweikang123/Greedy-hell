460163fd07b3d9469735950caeb924d4d739ebc4
xwk <570312124@qq.com>
2018-10-08 23:14:27 +0800

    开始游戏黑场过渡(1/4)

---
+++ Greedy hell/Assets/Scripts/LoadingScene/Loading.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Globe
+{
+    public static string loadSceneName;
+}
+
+public class Loading : MonoBehaviour
+{
+
+
+    public void StartGame(string sceneName)
+    {
+        if (sceneName == string.Empty)
+            sceneName = "Game";
+
+        Globe.loadSceneName = sceneName;
+
+        SceneManager.LoadScene("Transitions");
+    }
+}
