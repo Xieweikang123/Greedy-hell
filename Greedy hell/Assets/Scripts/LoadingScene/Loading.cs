a104502ed25b6735f9889d8b31b0b99ea50c0b08
xwk <570312124@qq.com>
2018-10-09 23:59:11 +0800

    开始游戏黑场过渡(2/4)

--- Greedy hell/Assets/Scripts/LoadingScene/Loading.cs
+++ Greedy hell/Assets/Scripts/LoadingScene/Loading.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Globe
 {
     public static string loadSceneName;
@@ -9,7 +10,36 @@ public class Globe
 
 public class Loading : MonoBehaviour
 {
+    //异步对象
+    private AsyncOperation async;
 
+    [SerializeField]
+    private Image blackMask;
+
+    bool m_Fading=false;
+
+    private void Update()
+    {
+        if (m_Fading == true)
+        {
+            //Fully fade in Image (1) with the duration of 2
+
+            blackMask.CrossFadeAlpha(1f, 3.0f, false);
+
+        }
+        ////If the toggle is false, fade out to nothing (0) the Image with a duration of 2
+        //if (m_Fading == false)
+        //{
+        //    blackMask.CrossFadeAlpha(0, 2.0f, false);
+
+        //}
+
+    }
+    void OnGUI()
+    {
+        //Fetch the Toggle's state
+        m_Fading = GUI.Toggle(new Rect(0, 0, 100, 30), m_Fading, "Fade In/Out");
+    }
 
     public void StartGame(string sceneName)
     {
@@ -18,6 +48,33 @@ public class Loading : MonoBehaviour
 
         Globe.loadSceneName = sceneName;
 
-        SceneManager.LoadScene("Transitions");
+        StartCoroutine(LoadScene());
+
+    }
+
+    IEnumerator LoadScene()
+    {
+        blackMask.gameObject.SetActive(true);
+
+        //异步读取场景
+        async = SceneManager.LoadSceneAsync(Globe.loadSceneName);
+
+        async.allowSceneActivation = false;
+
+        yield return Gradient();
+
+        if (async.progress == 0.9f)
+        {
+            async.allowSceneActivation = true;
+        }
+
+        yield return async;
+    }
+
+    IEnumerator Gradient()
+    {
+        m_Fading = true;
+
+        yield return  new WaitForSeconds(3f);
     }
 }
