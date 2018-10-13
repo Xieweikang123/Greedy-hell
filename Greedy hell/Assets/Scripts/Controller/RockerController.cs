a0fc65ffc5683118512b5f5f7e30afdc2fb59c95
xwk <570312124@qq.com>
2018-10-13 14:38:02 +0800

    虚拟摇杆(1.0)

---
+++ Greedy hell/Assets/Scripts/Controller/RockerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RockerController :MonoBehaviour, IDragHandler,IEndDragHandler
+{
+    private float JoyStickRadius = 50;   //摇杆最大半径
+
+    private RectTransform selfTransform;// 当前物体的Transform组件
+
+    private bool isTouched = false;// 是否触摸了虚拟摇杆
+
+    private Vector2 originPosition; //虚拟摇杆默认位置
+    //虚拟摇杆移动方向
+    private Vector2 touchedAxis;
+
+    public Vector2 TouchedAxis
+    {
+        get
+        {
+            if (touchedAxis.magnitude < JoyStickRadius)
+                return touchedAxis.normalized / JoyStickRadius;
+            return touchedAxis.normalized;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
